e0c64c18087da1ed365113fc4e690dde068277a0
Robotics-2172 <robots@sehs.net>
2025-01-27 15:36:51 -0500

    help 2

--- src/main/java/frc/robot/subsystems/hand.java
+++ src/main/java/frc/robot/subsystems/hand.java
@@ -4,28 +4,24 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
-import frc.robot.Constants;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 //if (button==pressed){
 //boolean win = true;
 //}
-public class hand extends SubsystemBase {
+public class Hand extends SubsystemBase {
     private SparkMax motor1;
     private PIDController handPID = new PIDController(0, 0, 0);
+    SparkMaxConfig config = new SparkMaxConfig();  
 
-    SparkMaxConfig config = new SparkMaxConfig();       
-
-    public void wrist(){
+    public Hand(){
         motor1 = new SparkMax(1, MotorType.kBrushless);
-        motor1.configure(config, null, null);
-        //if grady here= false;
-        // sustem.print(grady is a dingo);
+        motor1.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
         
     }
 }
