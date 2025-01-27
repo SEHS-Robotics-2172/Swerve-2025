0d7333bfcfca09a2f3a840a205ba4a2e58cd1b0f
Robotics-2172 <robots@sehs.net>
2025-01-27 16:24:33 -0500

    help4

--- src/main/java/frc/robot/subsystems/hand.java
+++ src/main/java/frc/robot/subsystems/hand.java
@@ -5,10 +5,11 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.*;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Encoder;
@@ -22,21 +23,45 @@ public class Hand extends SubsystemBase {
     private SparkMax intakeMotor1;
     private SparkMax intakeMotor2;
     private PIDController handPID = new PIDController(0, 0, 0);
+    double wantedPosition = 0;
     SparkMaxConfig wristConfig = new SparkMaxConfig();  
     SparkMaxConfig intakeCCW = new SparkMaxConfig();
     SparkMaxConfig intakeCW = new SparkMaxConfig();
-    private AbsoluteEncoder encoder;
+    AbsoluteEncoder encoder;
 
     public Hand(){
+        wristConfig.inverted(false);
+        intakeCCW.inverted(true);
+        intakeCW.inverted(false);
+
+        wristConfig.idleMode(IdleMode.kBrake);
+        intakeCCW.idleMode(IdleMode.kCoast);
+        intakeCW.idleMode(IdleMode.kCoast);
+
         wristMotor = new SparkMax(Constants.Hand.wristMotorID, MotorType.kBrushless);
         intakeMotor1 = new SparkMax(Constants.Hand.intakeMotor1ID, MotorType.kBrushless);
         intakeMotor2 = new SparkMax(Constants.Hand.intakeMotor2ID, MotorType.kBrushless);
 
-        wristMotor.configure(wristConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-        
+        wristMotor.configure(wristConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        intakeMotor1.configure(intakeCCW, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        intakeMotor2.configure(intakeCW, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         encoder = wristMotor.getAbsoluteEncoder();
 
-
     }
+    
+ @Override
+  public void periodic() {
+    handPID.setSetpoint(wantedPosition);
+    double wristSpeed = handPID.calculate(encoder.getPosition());
+    wristMotor.set(wristSpeed);
+
+  }
+  public void addWantedPosition(double rotations){
+    wantedPosition += (7.75 * rotations * handPID.getPeriod());
+  }
+  public void setWantedPosition(double rotations){
+    wantedPosition = rotations;
+  }
+
 }
