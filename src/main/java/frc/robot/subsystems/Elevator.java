b0fc2a769afe06f6d0ab8040099fa4c7dd128ece
Robotics-2172 <robots@sehs.net>
2025-02-18 16:33:26 -0500

    i hate you

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -14,6 +14,7 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import frc.robot.Constants;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -22,19 +23,19 @@ public class Elevator extends SubsystemBase {
   private TalonFX motor1;
   private TalonFX motor2;
   private SparkMax encoder;
-  private PositionVoltage elevatorPID = new PositionVoltage(0);
+  private PIDController elevatorController;
+  double kP;
+  double kI;
+  double kD;
   double wantedPosition = 0;
   TalonFXConfiguration config = new TalonFXConfiguration(); 
   public Elevator() {
+    kP = 0.2;
+    kI = 0.2;
+    kD = 0;
+    elevatorController = new PIDController(kP, kI, kD);
     config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-    config.Slot0.GravityType = GravityTypeValue.Elevator_Static;
     config.Feedback.SensorToMechanismRatio = 7.75 / 2;
-    config.Slot0.kI = 2;
-    config.Slot0.kP = 6;
-    config.Slot0.kD = 0;
-    config.Slot0.kV = 0;
-    config.Slot0.kA = 0;
-    config.Slot0.kG = 0.4;
     motor1 = new TalonFX(Constants.Elevator.motor1ID);
     motor2 = new TalonFX(Constants.Elevator.motor2ID);
     encoder = new SparkMax(Constants.Elevator.encoderID, MotorType.kBrushed);
@@ -46,9 +47,11 @@ public class Elevator extends SubsystemBase {
 
   @Override
   public void periodic() {
-    PositionVoltage speed = elevatorPID.withPosition(wantedPosition);
-    motor1.setControl(speed);
-    motor2.setControl(speed);
+    if (wantedPosition - motor1.getPosition().getValueAsDouble() <= 0){
+      elevatorController.setPID(kP/2, kI/2, kD);
+    } else {
+      elevatorController.setPID(kP, kI, kD);
+    }
     // SmartDashboard.putNumber("Elevator Absolute Position", getRotations()); 
     // SmartDashboard.putNumber("Elevator Position", motor1.getPosition().getValueAsDouble());
     // SmartDashboard.putNumber("Elevator Wanted Position", wantedPosition); 
